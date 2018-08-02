a9b2eec82bc066a5a05e616fe2e30a59aef17ce0
rudreshrm05 <rudramadhu0509@gmail.com>
2018-08-02 11:19:24 +0530

    the coolest testing app - on development

--- tester_app/Main_activity.java
+++ tester_app/Main_activity.java
@@ -7,11 +7,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
+import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.wifi.WifiEnterpriseConfig;
 import android.net.wifi.WifiManager;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
@@ -22,19 +28,28 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import static java.lang.System.exit;
+
 /**
  * Created by Rudresh on 25-07-2018.
  */
 
 public class Main_activity extends Activity{
 
-    Button indicator_bluetooth, indicator_wifi, indicator_dual_sim, indicator_signal_strength, indicator_nfc, indicator_headphone_jack, indicator_basic_apps;
-    LinearLayout bluetooth_Option, wifi_option, dual_sim_option, signal_strength_option, nfc_option, headphone_jack_option, basic_apps_option;
+    Button indicator_bluetooth, indicator_wifi, indicator_dual_sim, indicator_signal_strength, indicator_nfc, indicator_headphone_jack, indicator_basic_apps, indicator_speaker;
+    LinearLayout bluetooth_Option, wifi_option, dual_sim_option, signal_strength_option, nfc_option, headphone_jack_option, basic_apps_option, speaker_option;
     BluetoothAdapter BA;
     WifiManager wifi;
+    AudioManager audioManager;
     TelephonyInfo telephonyInfo;
+    MediaPlayer test_audio_file;
+    MediaRecorder test_sound;
     public int signalStrengthDbm = 0;
     public int signalStrengthAsuLevel = 0;
+    int bufferSize,j,i;
+    double lastLevel, audio_samples[]=new double[6];
+    AudioRecord audio;
+    Thread thread;
 
     final BroadcastReceiver broad_cast_receiver=new BroadcastReceiver() {
         @Override
@@ -79,6 +94,8 @@ public class Main_activity extends Activity{
         indicator_headphone_jack=(Button)findViewById(R.id.indicator_headphone_jack);
         basic_apps_option=(LinearLayout)findViewById(R.id.basic_apps_option);
         indicator_basic_apps=(Button)findViewById(R.id.indicator_basic_apps);
+        speaker_option=(LinearLayout)findViewById(R.id.speaker_option);
+        indicator_speaker=(Button)findViewById(R.id.indicator_speaker);
 
 
         final PhoneStateListener phone_state_listener=new PhoneStateListener(){
@@ -225,7 +242,6 @@ public class Main_activity extends Activity{
             public void onClick(View v) {
                 int app_count=0, mpi=0;
                 boolean isPackageExist;
-                String s="";
                 String[] missing_packages=new String[8];
                 String[] package_names={getString(R.string.settings), getString(R.string.playstore), getString(R.string.google),getString(R.string.gmail), getString(R.string.google_maps), getString(R.string.calculator), getString(R.string.calendar), getString(R.string.messaging)};
                 List<ApplicationInfo> packages = Main_activity.this.getPackageManager().getInstalledApplications(0);
@@ -248,7 +264,68 @@ public class Main_activity extends Activity{
                 else indicator_basic_apps.setBackgroundResource(R.drawable.test_fail);
             }
         });
+
+        speaker_option.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                test_audio_file = MediaPlayer.create(Main_activity.this, R.raw.speaker_test);
+                audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+                int sampleRate = 8000;
+                bufferSize = AudioRecord.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+                audio = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,5,0);
+
+                test_audio_file.start();
+                audio.startRecording();
+
+                j=0;
+                thread = new Thread(new Runnable() {
+                    public void run() {
+                        for(i=0;i<6;i++){
+                            try{Thread.sleep(2000);}catch(InterruptedException ie){ie.printStackTrace();}
+                            readAudioBuffer();//After this call we can get the last value assigned to the lastLevel variable
+
+                            runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    audio_samples[j]=lastLevel;
+                                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,50,0);
+                                    j++;
+                                    if(i==5){
+										if(audio_samples[1]>=audio_samples[4]){
+											indicator_speaker.setBackgroundResource(R.drawable.test_ok);
+										}
+										else{
+											indicator_speaker.setBackgroundResource(R.drawable.test_ok);
+										}
+									}
+                                }
+                            });
+                        }
+                    }
+                });
+                thread.start();
+            }
+        });
     }
+
+   private void readAudioBuffer() {
+        try {
+            short[] buffer = new short[bufferSize];
+            int bufferReadResult = 1;
+
+            if (audio != null) {
+                // Sense the voice…
+                bufferReadResult = audio.read(buffer, 0, bufferSize);
+                double sumLevel = 0;
+                for (int i = 0; i < bufferReadResult; i++)
+                { sumLevel += buffer[i]; }
+                lastLevel = Math.abs((sumLevel / bufferReadResult)); }
+        } catch (Exception e) { e.printStackTrace(); }
+   }
 }
 
 
